5ea0f158c5855f4de9722bbc797240737d28a577
Muskan <tanyaverma959@gmail.com>
2023-08-16 13:06:29 +0530

    added sol

--- Assignment 5/Day5_Practice.sql
+++ Assignment 5/Day5_Practice.sql
@@ -1,6 +1,66 @@
+--CREATE TABLE RETURNS
 CREATE TABLE returns (
      "Order Id" VARCHAR(50) PRIMARY KEY,
 	"Return Reason" VARCHAR(50)
 
 );
+
+--Create Table Employee 
+create table employee(
+    emp_id int,
+    emp_name varchar(20),
+    dept_id int,
+    salary int,
+    manager_id int,
+    emp_age int
+);
+
+--Inserting Data in employee
+
+insert into employee values(1,'Ankit',100,10000,4,39);
+insert into employee values(2,'Mohit',100,15000,5,48);
+insert into employee values(3,'Vikas',100,10000,4,37);
+insert into employee values(4,'Rohit',100,5000,2,16);
+insert into employee values(5,'Mudit',200,12000,6,55);
+insert into employee values(6,'Agam',200,12000,2,14);
+insert into employee values(7,'Sanjay',200,9000,2,13);
+insert into employee values(8,'Ashish',200,5000,2,12);
+insert into employee values(9,'Mukesh',300,6000,6,51);
+insert into employee values(10,'Rakesh',500,7000,6,50);
+
+--CREATE TABLE DEPARTMENTS
+create table dept(
+    dep_id int,
+    dep_name varchar(20)
+);
+
+--INSERTING DATA IN DEPARTMENTS
+insert into dept values(100,'Analytics');
+insert into dept values(200,'IT');
+insert into dept values(300,'HR');
+insert into dept values(400,'Text Analytics');
+
+--SELECT STATEMENTS
+select * from dept;
+select * from employee;
+SELECT * FROM returns;
+SELECT * FROM superstore_orders;
+
+--Renaming Column names
+ALTER TABLE returns
+RENAME COLUMN "Order Id" TO order_id;
+
+ALTER TABLE returns
+RENAME COLUMN "Return Reason" TO return_reason;
+
+--query to get region wise count of return orders
+select region, COUNT(distinct o.order_id) as returned_ord
+from superstore_orders o
+inner join returns r on o.order_id = r.order_id
+group by region;
+
+
+
+
+
 
--- Assignment 5/NS_DAY5_Notes.sql
+++ Assignment 5/NS_DAY5_Notes.sql
@@ -14,17 +14,18 @@ order by student_id;
 --create table returns (order_id  varchar(10),return_reason varchar(10))
 -- database joins
 select o.order_id,o.product_id,r.return_reason
-from orders o
+from superstore_orders o
 inner join returns r on o.order_id=r.order_id;
 
 
 select o.order_id,o.product_id,r.return_reason, r.order_id as return_order_id
-from orders o
+from superstore_orders o
 left join returns r on o.order_id=r.order_id
 ;
 
+
 select r.return_reason,sum(sales) as total_sales
-from orders o
+from superstore_orders o
 inner join returns r on o.order_id=r.order_id
 group by r.return_reason;
 
@@ -53,7 +54,7 @@ select * from employee;
 select * from dept;
 
 select o.order_id,o.product_id,r.return_reason,p.manager
-from orders o
+from superstore_orders o
 left join returns r on o.order_id=r.order_id
 inner join people p on o.region=p.region
 ;
@@ -63,7 +64,7 @@ create table people
 (
 manager varchar(20),
 region varchar(10)
-)
+);
 
 insert into people
 values ('Ankit','West')
